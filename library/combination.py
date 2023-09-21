215586d17e0adebec10598c640728d23ffda5f71
bong <sbskh11@gmail.com>
2023-09-21 09:03:48 +0900

    add combination nd permutation

---
+++ library/combination.py
@@ -0,0 +1,35 @@
+def gen_combinations(arr, n):
+    result =[] 
+
+    if n == 0: 
+        return [[]]
+
+    for i in range(0, len(arr)): 
+        elem = arr[i] 
+        rest_arr = arr[i + 1:] 
+        for C in gen_combinations(rest_arr, n-1): 
+            result.append([elem]+C) 
+              
+    return result
+
+def gen_permutations(arr, n):
+    result = []
+
+    if n == 0:
+        return [[]]
+
+    for i, elem in enumerate(arr): 
+        for P in gen_permutations(arr[:i] + arr[i+1:], n-1):
+            result += [[elem]+P]
+              
+    return result
+
+def permutations_4(array, r):
+    for i in range(len(array)):
+        if r == 1:
+            yield [array[i]]
+        else:
+            for next in permutations_4(array, r-1):
+                yield [array[i]] + next
+
+print(gen_permutations([1,2,3],3))
