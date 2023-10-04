a5da08815c0d2b15329df6ca9a3b15cc503cd53d
bong <sbskh11@gmail.com>
2023-10-05 08:28:39 +0900

    add combination_permutation example

--- example/use_example/combination_permutation.py
+++ example/use_example/combination_permutation.py
@@ -1,3 +1,5 @@
+from itertools import permutations, product
+
 def gen_combinations(arr, n):
     result =[] 
 
@@ -12,40 +14,21 @@ def gen_combinations(arr, n):
               
     return result
 
-def gen_permutations(arr, n):
-    result = []
-
-    if n == 0:
-        return [[]]
-
-    for i, elem in enumerate(arr): 
-        for P in gen_permutations(arr[:i] + arr[i+1:], n-1):
-            result += [[elem]+P]
-              
-    return result
-
-def permutations_4(array, r):
-    for i in range(len(array)):
-        if r == 1:
-            yield [array[i]]
-        else:
-            for next in permutations_4(array, r-1):
-                yield [array[i]] + next
-
-from itertools import permutations, product
-
 arr_sign = ['+','-']
-arr = [1,2,3]
+arr = [1,2,3,1]
 len_arr = len(arr)
 target_number = 5
 
 permutation_arr_sign = list(product(arr_sign, repeat=len_arr))
-premutation_arr = gen_permutations(arr,len_arr)
+combinations_arr = gen_combinations(arr,len_arr)
+
+print(permutation_arr_sign)
+print(combinations_arr)
 
 list_solution_containner = []
 sign_ele_number = 0
 
-for arr_ in premutation_arr:
+for arr_ in combinations_arr:
     for sign_ in permutation_arr_sign:
         cal = 0
         list_solution_containner_tmp = []
@@ -59,12 +42,10 @@ for arr_ in premutation_arr:
             list_solution_containner_tmp.append(sign_[idx])
             list_solution_containner_tmp.append(arr_ele)
 
-            if cal > target_number:
-                break
-
-            elif cal == target_number:
-                if list_solution_containner_tmp not in list_solution_containner:
-                    list_solution_containner.append(list_solution_containner_tmp)
+        if cal == target_number:
+            print(cal)
+            print(list_solution_containner_tmp)
+            list_solution_containner.append(list_solution_containner_tmp)
             
 
 print(len(list_solution_containner))
