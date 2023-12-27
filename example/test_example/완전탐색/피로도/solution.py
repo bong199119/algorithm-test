53d2b48f2dd6c5b3e5116b1da2e6dc03701d80b0
bong <sbskh11@gmail.com>
2023-12-27 13:50:58 +0900

    피로도

--- example/test_example/완전탐색/피로도/solution.py
+++ example/test_example/완전탐색/피로도/solution.py
@@ -1,22 +1,50 @@
 import itertools
+import time
 
+start_time = time.time()
 def make_permutation(dungeons):
+    list_dungeons = []
     for i in range(len(dungeons)):
-        itertools.permutations()
-
-def canPass_dungeon(list_dungeons):
+        list_dungeons += list(itertools.permutations(dungeons, i + 1))
 
     return list_dungeons
+
+def canPass_dungeon(k, list_dungeons):
+    list_dungeons_passed = []
+
+    for dungeons in list_dungeons:
+        k_forpass = k
+        pass_num = 0
+        for dungeon in dungeons:
+            if k_forpass >= dungeon[0]:
+                k_forpass -= dungeon[1] 
+                if k_forpass >= 0:
+                    pass_num += 1
+        if pass_num == len(dungeons):
+            list_dungeons_passed.append(dungeons)
+
+    return list_dungeons_passed
 
 def solution(k, dungeons):
     answer = -1
 
     list_dungeons = make_permutation(dungeons)
-    list_dungeons_passed = canPass_dungeon(list_dungeons)
+    list_dungeons_passed = canPass_dungeon(k, list_dungeons)
 
     for dungeons_passed in list_dungeons_passed:
         if answer < len(dungeons_passed):
             answer = len(dungeons_passed)
 
     return answer
+
+
+dungeons = [[80,20],[50,40],[30,10],[80,20],[50,40],[10,40],[10,40],[10,40]]
+k = 80
+
+answer = solution(k, dungeons)
+print(answer)
+
+end_time = time.time()
+print(start_time, end_time)
+print('take_time : ', end_time - start_time)
 
---
+++ example/test_example/완전탐색/피로도/solution2.py
@@ -0,0 +1,33 @@
+import time
+start_time = time.time()
+
+def dfs(dungeons):
+    dungeons_new = [dungeons[:][:]]
+
+
+
+
+def solution(k, dungeons):
+    answer = -1
+
+    return answer
+
+
+
+
+dungeons = [[80,20],[50,40],[30,10],[80,20],[50,40],[10,40],[10,40],[10,40]]
+# k = 80
+
+# answer = solution(k, dungeons)
+# print(answer)
+
+# end_time = time.time()
+# print(start_time, end_time)
+# print('take_time : ', end_time - start_time)
+
+dungeons_new = [dungeons[:][:]]
+print(dungeons_new)
+
+dungeons_new[0][0][1] = 200
+
+print(dungeons)
