9641b3250b1470c1358a60440fc1bb7cc3045530
bong <sbskh11@gmail.com>
2024-02-02 14:26:10 +0900

    index_sort 베스트 엘범

--- example/test_example/해시/베스트앨범/solution.py
+++ example/test_example/해시/베스트앨범/solution.py
@@ -1,16 +1,27 @@
-def sort_genres(dict_genres):
-    list_genre = []
+def sort_genres(list_genre, dict_genres):
+    for i in range(len(list_genre) - 1):
+        for j in range(len(list_genre) - i - 1):
+            if dict_genres[list_genre[j]] > dict_genres[list_genre[j+1]]:
+                list_genre[j], list_genre[j+1] = list_genre[j+1], list_genre[j]
+    
     return list_genre
 
 def sort_plays(list_plays):
+    for i in range(len(list_plays) - 1):
+        for j in range(len(list_plays) - i - 1):
+            if list_plays[j][0] > list_plays[j+1][0]:
+                list_plays[j], list_plays[j+1] = list_plays[j+1], list_plays[j]
+
     return list_plays
 
 def solution(genres, plays):
     answer = []
     dict_genres = {}
+    list_genre = []
 
     for idx, play in enumerate(plays):
         if genres[idx] not in dict_genres:
+            list_genre.append(genres[idx])
             dict_genres[genres[idx]] = {}
             dict_genres[genres[idx]]['plays'] = [[play, idx]]
             dict_genres[genres[idx]]['total plays'] = play
@@ -19,9 +30,9 @@ def solution(genres, plays):
             dict_genres[genres[idx]]['plays'].append([play, idx])
             dict_genres[genres[idx]]['total plays'] += play
 
-    list_genre = sort_genres(dict_genres)
+    list_genre_sorted = sort_genres(list_genre, dict_genres)
 
-    for genre in list_genre:
+    for genre in list_genre_sorted:
         list_plays = dict_genres[genre]['plays']
         list_plays = sort_plays(list_plays)
 
